54751de29997948435a4b8d8303c27cc89122307
Breanna-J <kalebandbre@gmail.com>
2026-02-11 17:13:35 -0600

    created balances module

---
+++ src/balances.rs
@@ -0,0 +1,9 @@
+//This is a ballances module.
+//This module keeps track of track of the balance of each account in the system.
+
+
+// a struct is an object that can hold multiple values of different types.
+pub struct Pallet{
+    // storage mapping the account ('String') to the balance (u128).
+    
+}
