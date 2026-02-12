3e79a2c3a3b388fbd3eff7e4149849f582648ed5
Breanna-J <kalebandbre@gmail.com>
2026-02-11 18:14:07 -0600

    created impl in balances

--- src/balances.rs
+++ src/balances.rs
@@ -1,9 +1,26 @@
 //This is a ballances module.
 //This module keeps track of track of the balance of each account in the system.
 
-
+//Need to import the BTreeMap from the standard library to use it in our Pallet struct.
+use std::collections::BTreeMap;
+#[derive(Debug, Clone)]
 // a struct is an object that can hold multiple values of different types.
 pub struct Pallet{
     // storage mapping the account ('String') to the balance (u128).
-    
+    account: String,
+    balance: u128,
+    balances: BTreeMap<String, u128>
+
+}
+
+
+
+impl Pallet {
+    pub fn new() -> Self {
+        Self {
+            account: String::new(),
+            balance: 0,
+            balances: BTreeMap::new() 
+            }
+    }
 }
