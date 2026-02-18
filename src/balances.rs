68fbdcfbfc52713eb23ce0658577ea0ddfdaaf90
Breanna-J <kalebandbre@gmail.com>
2026-02-17 22:03:19 -0600

    making the first fun of runtime

--- src/balances.rs
+++ src/balances.rs
@@ -1,23 +1,21 @@
 //This is a ballances module.
 //This module keeps track of track of the balance of each account in the system.
-
 //Need to import the BTreeMap from the standard library to use it in our Pallet struct.
 
+
 use std::collections::BTreeMap;
+use num_traits::{Zero, CheckedAdd, CheckedSub};
 
-//We can make the types explicit, this makes it easier to reffactor and change the overall type
-type Account = String;
-type Balance = u128;
-
-// a struct is an object that can hold multiple values of different types, like a class.
-#[derive(Debug, Clone)]
-pub struct Pallet{
+pub struct Pallet<Account, Balance>{
     // storage mapping the account ('String') to the balance (u128).
-    balances: BTreeMap<String, u128>
+    balances: BTreeMap<Account, Balance>
 
 }
-
-impl Pallet {
+impl <Account, Balance> Pallet<Account, Balance>
+where 
+    Account: Ord + Clone, //orderable and dublicatable
+    Balance: Zero + CheckedSub + CheckedAdd + Copy, //can be zero, can support safe arrethmatic, cheap to duplicate without moving
+{
     pub fn new() -> Self {
         Self {
             balances: BTreeMap::new() 
@@ -26,8 +24,8 @@ impl Pallet {
 
     //set the balance of an account.
 
-    pub fn set_balance(&mut self, account:Account, balance: Balance) {
-        self.balances.insert(account, balance);
+    pub fn set_balance(&mut self, account:&Account, balance: Balance) {
+        self.balances.insert(account.clone(), balance);
     }
 
     //get the balance of an account.
@@ -35,16 +33,16 @@ impl Pallet {
     //the return type is Option<&u128> because we want to return a reference
     //to the balance if it exists, or None if it does not exist.
 
-    pub fn get_balance(&self, account: &String) -> u128 {
-        *self.balances.get(account).unwrap_or(&0)
+    pub fn get_balance(&self, account: &Account) -> Balance{
+        *self.balances.get(account).unwrap_or(&Balance::zero())
     }
     //function to transfer balance from one account to another.
     pub fn transfer(
         &mut self,
-        caller: String,
-        to: String,
-        amount: u128,
-    ) -> Result<(), String> {
+        caller: Account,
+        to: Account,
+        amount:Balance,
+    ) -> Result<(), &'static str> {
 
             //check the sender has enough balance to transfer.
             let caller_balance = self.get_balance(&caller);
@@ -52,10 +50,10 @@ impl Pallet {
             
             //check if the caller has enough balance to transfer the amount, and if the recipient's balance will not overflow after the transfer
             //perform the math safely using checked_sub and checked_add to prevent overflow and underflow, and return an error if the math is not correct.
-            let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Insufficient balance".to_string())?;
+            let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Insufficient balance")?;
             
             //? means if the result is an error, return the error immediately, otherwise continue with the value.
-            let new_to_balance = to_balance.checked_add(amount).ok_or("Balance overflow".to_string())?;
+            let new_to_balance = to_balance.checked_add(&amount).ok_or("Balance overflow")?;
 
             //if the caller has enough balance, transfer the amount from the caller to the recipient and update both balances.
             self.set_balance(caller, new_caller_balance);
@@ -69,6 +67,8 @@ impl Pallet {
 //this is a conditional compilation attribute that tells the compiler to only compile when running tests. This is useful for keeping test code separate from production code.
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     //singular test
     #[test]
 
@@ -84,14 +84,17 @@ mod tests {
         
         //self=> the current module, super=> the parent module(current file), crate=> the root module(parent directory.
         
-        let mut pallet = super::Pallet::new();
+        let mut balances: Pallet<String, u128> = super::Pallet<String, u128>::new();
 
-        assert_eq!(pallet.set_balance("Alice".to_string(), 0), ());
-        assert_eq!(pallet.set_balance("Bob".to_string(), 0), ());
-        pallet.set_balance("Alice".to_string(), 100);
+        pallet.set_balance("Alice".to_string(), 0);
+        pallet.set_balance("Bob".to_string(), 0);
+
+        let res = pallet.transfer("Alice".to_string(), "Bob".to_string, 40);
+        assert_eq!(res, Ok(()));
+
         assert_eq!(pallet.get_balance(&"Alice".to_string()), 100);
-        assert_eq!(pallet.get_balance(&"Bob".to_string()), 0);
+        assert_eq!(pallet.get_balance(&"Bob".to_string()), 60);
     }
 
 
-}
+}   
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ mod system;
 use system::Pallet as SystemPallet;
 use balances::Pallet as BalancesPallet;
 
+//We can make the types explicit, this makes it easier to reffactor and change the overall type/constrain the code
+pub mod types{
+    pub type Account = String;
+    pub type Balance = u128;
+}
+
 //asks rust to allow you to print the type  in human readible text for debugging; all of the pallets associated need the derive(debug) statement
 #[derive(Debug)]
 
@@ -13,20 +19,47 @@ use balances::Pallet as BalancesPallet;
 //struct is the definition of the data
 pub struct Runtime {
     //SystemPallet and BalancePallet are imported above in the use statement
-    system : SystemPallet,
-    balances : BalancesPallet,
+    system : system::Pallet,
+    balances : balances::Pallet<types::Account, types::Balance>,
 }
 //impl is what it can do
 impl Runtime {
     //create an instance of the main runtime by creating new instatnces of the pallets
     fn new()-> Self{
         Self { 
-            system: SystemPallet::new(), 
-            balances: BalancesPallet::new() 
+            system: system::Pallet::new(), 
+            balances: balances::Pallet::new() 
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    //start the runtime
+    let mut runtime: Runtime = Runtime::new();
+
+    //initalize the accounts
+    let alice: String = "Alice".to_string();
+    let bob: String = "Bob".to_string();
+    let charlie: String = "Charlie".to_string();
+
+    //set balance for Alice
+    runtime.balances.set_balance(&alice, 100);
+
+    //start emulating the block
+    runtime.system.increment_block();
+    assert_eq!(runtime.system.block_number(), 1);
+
+    //first transaction
+    runtime.system.increment_nonce(&alice);
+    let _res: Result<(), ()> = runtime.balances.transfer(alice.clone(), bob,30).map_err( |e| eprintln!("{e}"));
+
+    //second transaction
+    runtime.system.increment_nonce(&alice);
+    let _res: Result<(), ()> = runtime.balances.transfer(alice.clone(), charlie,15).map_err( |e| eprintln!("{e}"));
+
+
+
+
+
 }
+ 
